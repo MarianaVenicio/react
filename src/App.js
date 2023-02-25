5627becf43ff22f2d67cf00b03a9a495f8c588ff
mariana venicio <marianaandreavenicio@hotmail.com>
2023-02-25 13:32:40 -0300

    tercero

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
-
+import CartWidget from "./Components/CartWidget/CartWidget";
+import Footer from "./Components/Footer/Footer";
+import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
+import Navbar from "./Components/Navbar/Navbar";
+import ProductCard from "./Components/ProductCard/ProductCard";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hola mundo!</h1>
+    <div>
+      <Navbar />
       
+      <ItemListContainer />
+      <ProductCard title= {"producto uno"} price={200} isRed={false} />
+      <ProductCard title= {"producto 2" }price={500} isRed= {true} />  
+             
+      
+      <Footer/>
     </div>
+  
   );
 }
 
